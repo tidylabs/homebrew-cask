ca8cf11e9028ab74943b75577272ae3c1704429a
Justin Klaassen <jck112@users.noreply.github.com>
2022-11-20 11:23:34 -0800

    Add balenaetcher 1.10.0

---
+++ Casks/balenaetcher.rb
@@ -0,0 +1,26 @@
+cask "balenaetcher" do
+  version "1.10.0"
+  sha256 "1d863ee377e997d66f7017320c294f6364ede947aee6c031323e72890ed5b683"
+
+  url "https://github.com/balena-io/etcher/releases/download/v#{version}/balenaEtcher-#{version}.dmg",
+      verified: "github.com/balena-io/etcher/"
+  name "Etcher"
+  desc "Tool to flash OS images to SD cards & USB drives"
+  homepage "https://balena.io/etcher"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "balenaEtcher.app"
+
+  uninstall quit: "io.balena.etcher.*"
+
+  zap trash: [
+    "~/Library/Application Support/balena-etcher",
+    "~/Library/Preferences/io.balena.etcher.helper.plist",
+    "~/Library/Preferences/io.balena.etcher.plist",
+    "~/Library/Saved Application State/io.balena.etcher.savedState",
+  ]
+end
